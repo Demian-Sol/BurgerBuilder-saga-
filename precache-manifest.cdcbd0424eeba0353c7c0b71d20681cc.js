32c1670fdd9ac8e11aef97354e5a82622a5abb01
Demian-Sol <demian_messages@mail.ru>
2018-12-18 18:46:58 +0700

    Updates

---
+++ precache-manifest.cdcbd0424eeba0353c7c0b71d20681cc.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "52877e29568d4e2822d2",
+    "url": "/BurgerBuilder-saga-/static/css/3.a38e1c87.chunk.css"
+  },
+  {
+    "revision": "ae32af3e82fc401ebeef",
+    "url": "/BurgerBuilder-saga-/static/css/main.fa954af3.chunk.css"
+  },
+  {
+    "revision": "89d1a66630a9f3095d6c93a4d3b85bce",
+    "url": "/BurgerBuilder-saga-/static/media/Burger-cat-logo.89d1a666.png"
+  },
+  {
+    "revision": "c725e8437e5bef5844cf",
+    "url": "/BurgerBuilder-saga-/static/js/1.c725e843.chunk.js"
+  },
+  {
+    "revision": "eef76e30c124db6ce075",
+    "url": "/BurgerBuilder-saga-/static/js/runtime~main.eef76e30.js"
+  },
+  {
+    "revision": "6f2dcf0a460ba7047c20",
+    "url": "/BurgerBuilder-saga-/static/js/2.6f2dcf0a.chunk.js"
+  },
+  {
+    "revision": "ae32af3e82fc401ebeef",
+    "url": "/BurgerBuilder-saga-/static/js/main.ae32af3e.chunk.js"
+  },
+  {
+    "revision": "52877e29568d4e2822d2",
+    "url": "/BurgerBuilder-saga-/static/js/3.52877e29.chunk.js"
+  },
+  {
+    "revision": "651f316789bb6f3525d4",
+    "url": "/BurgerBuilder-saga-/static/js/4.651f3167.chunk.js"
+  },
+  {
+    "revision": "6f2dcf0a460ba7047c20",
+    "url": "/BurgerBuilder-saga-/static/css/2.006ae4d2.chunk.css"
+  },
+  {
+    "revision": "c725e8437e5bef5844cf",
+    "url": "/BurgerBuilder-saga-/static/css/1.a9b058b6.chunk.css"
+  },
+  {
+    "revision": "81cafe0b3e524748c170875249fff51f",
+    "url": "/BurgerBuilder-saga-/index.html"
+  }
+];
